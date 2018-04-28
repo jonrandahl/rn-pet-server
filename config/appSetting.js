77787fc93658275c796720476ca955d95352872c
congli <admin@lee5214.com>
2018-04-27 21:32:17 -0700

    done, data updated

--- config/appSetting.js
+++ config/appSetting.js
@@ -1,1 +1,1 @@
-module.exports = {  firestore:{    docAmountPerBatch: 400,    petsCollectionName: 'test',    rescueGroupAPIKey : 'KJquDhsf'  }}
+module.exports = {  firestore:{    docAmountPerBatch: 480,    petsCollectionName: 'pets',    rescueGroupAPIKey : 'KJquDhsf'  }}
